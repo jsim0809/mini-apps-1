47be543199d2d4747f20891c2accd6b4c8495328
jsim0809 <jeremy_sim@hotmail.com>
2020-03-03 12:04:34 -0800

    Semi-working form. Receives input and partially parses into CSV file.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,33 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const path = require('path');
+const bodyParser = require('body-parser');
 
 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/', express.static(path.join(__dirname, 'client')));
+
+
+app.use(bodyParser.json());
+
+app.post('/generate', (req, res) => {
+  console.log('Received POST request.');
+  console.log(req.body.input);
+  res.send(`<p>${generateCSV(req.body.input)}</p>`);
+});
+
+app.listen(PORT, () => console.log(`Jeremy's CSV Report Generator listening on port ${PORT}.`));
+
+
+//// CSV Generator function
+// Takes a JSON string and returns a CSV string
+
+var generateCSV = string => {
+  var jsonObj = JSON.parse(string);
+  var csv = '';
+  csv += Object.keys(jsonObj);
+  return csv;
+};
