1266e7c36c17c975df1d040af8beb8864976e327
Jeremy Sim <jsim0809@gmail.com>
2020-03-04 12:51:37 -0800

    One final bug in victory detection

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -76,17 +76,14 @@ class App extends React.Component {
       for (var i = 0; i < board.length; i++) {
         for (var j = 0; j < 6; j++) {
           if (board[i][j] === letter) {
-            if ((board[i + 1][j + 1] === letter && board[i + 2][j + 2] === letter && board[i + 3][j + 3] === letter) ||
-              (board[i - 1][j - 1] === letter && board[i - 2][j - 2] === letter && board[i - 3][j - 3] === letter)) {
+            if ((board[i - 1][j + 1] === letter && board[i - 2][j + 2] === letter && board[i - 3][j + 3] === letter) ||
+              (board[i + 1][j - 1] === letter && board[i + 2][j - 2] === letter && board[i + 3][j - 3] === letter)) {
               result = true;
             }
           }
         }
       }
     });
-
-
-
     return result;
   }
 
