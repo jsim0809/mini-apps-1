e6e99006510ca849e91538b7712e20e58e2a1cc5
jsim0809 <jeremy_sim@hotmail.com>
2020-03-04 12:37:03 -0800

    Finally set up Babel and React.

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,35 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 0
+    };
+  }
+
+  handleClick() {
+    alert('hi');
+  }
+
+  render() {
+    switch(this.state.page) {
+      case 0:
+        return (
+          <button onClick={this.handleClick}>Checkout</button>
+        );
+        break;
+      case 1:
+        return
+        break;
+      case 2:
+        return
+        break;
+      case 3:
+        return
+        break;
+    }
+
+  }
+}
 
 
+ReactDOM.render(<App />, document.getElementById("app"));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
+const path = require('path');
 
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
