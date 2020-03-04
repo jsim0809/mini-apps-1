8ce66c2a06f1f1884a91f06bc9eb736478a3e782
jsim0809 <jeremy_sim@hotmail.com>
2020-03-04 15:34:25 -0800

    Client-side functionality complete

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -10,6 +10,8 @@ class App extends React.Component {
   }
 
   handleClick() {
+    // Send state object to server
+    // Clear fields
     this.setState(oldState => {
       if (oldState.page === 4) {
         return { page: 0 };
@@ -20,8 +22,9 @@ class App extends React.Component {
   }
 
   handleChange(event) {
+    var name = event.target.name;
     this.setState({
-      test: event.target.value
+      [name]: event.target.value
     })
   }
 
@@ -42,20 +45,19 @@ class App extends React.Component {
         return (
           <div>
             <h4>Please create an account:</h4>
-
-              <label for="name">Name </label>
-              <input type="text" name="name" onChange={this.handleChange} />
-              <br />
-              <br />
-              <label for="name">Email </label>
-              <input type="email" name="email" onChange={this.handleChange} />
-              <br />
-              <br />
-              <label for="name">Password </label>
-              <input type="password" name="password" onChange={this.handleChange} />
-              <br />
-              <br />
-              <button onClick={this.handleClick}>Next</button>
+            <label for="name">Name </label>
+            <input type="text" name="name" onChange={this.handleChange} />
+            <br />
+            <br />
+            <label for="email">Email </label>
+            <input type="email" name="email" onChange={this.handleChange} />
+            <br />
+            <br />
+            <label for="email">Password </label>
+            <input type="password" name="password" onChange={this.handleChange} />
+            <br />
+            <br />
+            <button onClick={this.handleClick}>Next</button>
           </div>
         );
         break;
@@ -63,6 +65,30 @@ class App extends React.Component {
         return (
           <div>
             <h4>Shipping details:</h4>
+            <label for="addr1">Address (line 1) </label>
+            <input type="text" name="addr1" onChange={this.handleChange} />
+            <br />
+            <br />
+            <label for="addr2">Address (line 2) </label>
+            <input type="text" name="addr2" onChange={this.handleChange} />
+            <br />
+            <br />
+            <label for="city">City </label>
+            <input type="text" name="city" onChange={this.handleChange} />
+            <br />
+            <br />
+            <label for="state">State </label>
+            <input type="text" name="state" onChange={this.handleChange} />
+            <br />
+            <br />
+            <label for="zip">Zip code </label>
+            <input type="text" name="zip" onChange={this.handleChange} />
+            <br />
+            <br />
+            <label for="phone">Phone </label>
+            <input type="tel" name="phone" onChange={this.handleChange} />
+            <br />
+            <br />
             <button onClick={this.handleClick}>Next</button>
           </div>
         );
@@ -71,6 +97,22 @@ class App extends React.Component {
         return (
           <div>
             <h4>Payment details:</h4>
+            <label for="cc">Credit card number </label>
+            <input type="text" name="cc" onChange={this.handleChange} />
+            <br />
+            <br />
+            <label for="expiry">Expiry date </label>
+            <input type="date" name="expiry" onChange={this.handleChange} />
+            <br />
+            <br />
+            <label for="cvv">CVV </label>
+            <input type="password" name="cvv" onChange={this.handleChange} />
+            <br />
+            <br />
+            <label for="bzip">Billing zip code </label>
+            <input type="text" name="bzip" onChange={this.handleChange} />
+            <br />
+            <br />
             <button onClick={this.handleClick}>Next</button>
           </div>
         );
@@ -79,6 +121,24 @@ class App extends React.Component {
         return (
           <div>
             <h4>Confirmation page</h4>
+            <br />
+            <h5>You entered: </h5>
+            <p>Name: {this.state.name}</p>
+            <p>Email: {this.state.email}</p>
+            <p>Password:  <em>hidden</em></p>
+            <br />
+            <p>Address (line 1): {this.state.addr1}</p>
+            <p>Address (line 2): {this.state.addr2}</p>
+            <p>City: {this.state.city}</p>
+            <p>State: {this.state.state}</p>
+            <p>Zip code: {this.state.zip}</p>
+            <p>Phone: {this.state.phone}</p>
+            <br />
+            <p>Credit card number: {this.state.cc}</p>
+            <p>CVV:  <em>hidden</em></p>
+            <p>Billing zip code: {this.state.bzip}</p>
+            <br />
+            <br />
             <button onClick={this.handleClick}>Purchase</button>
           </div>
         );
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -6,4 +6,5 @@ const path = require('path');
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
