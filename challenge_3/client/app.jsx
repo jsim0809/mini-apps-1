ead539f3ca200eb8d8e2b231b5858e13eac9b372
jsim0809 <jeremy_sim@hotmail.com>
2020-03-04 13:49:13 -0800

    Set up form pages 1, 2, 3 & confirmation to draw on button press

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -7,24 +7,53 @@ class App extends React.Component {
   }
 
   handleClick() {
-    alert('hi');
+    this.setState(oldState => {
+      if (oldState.page === 4) {
+        return { page: 0 };
+      } else {
+        return { page: oldState.page + 1 }
+      }
+    });
   }
 
   render() {
-    switch(this.state.page) {
+    switch (this.state.page) {
       case 0:
         return (
-          <button onClick={this.handleClick}>Checkout</button>
+          <button onClick={this.handleClick.bind(this)}>Checkout</button>
         );
         break;
       case 1:
-        return
+        return (
+          <div>
+            <div>Form page 1</div>
+            <button onClick={this.handleClick.bind(this)}>Next</button>
+          </div>
+        );
         break;
       case 2:
-        return
+        return (
+          <div>
+            <div>Form page 2</div>
+            <button onClick={this.handleClick.bind(this)}>Next</button>
+          </div>
+        );
         break;
       case 3:
-        return
+        return (
+          <div>
+            <div>Form page 3</div>
+            <button onClick={this.handleClick.bind(this)}>Next</button>
+          </div>
+        );
+        break;
+      case 4:
+        return (
+          <div>
+            <div>Confirmation Page</div>
+            <button onClick={this.handleClick.bind(this)}>Purchase</button>
+          </div>
+        );
         break;
     }
 
